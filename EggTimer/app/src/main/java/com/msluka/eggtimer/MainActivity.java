6c672b092fef1f3e8048beba508b20ab4a12c0c2
Msluka <mamulashvilis@gmail.com>
2019-01-09 23:38:37 +0100

    Egg Timer Initial commit

---
+++ EggTimer/app/src/main/java/com/msluka/eggtimer/MainActivity.java
@@ -0,0 +1,114 @@
+package com.msluka.eggtimer;
+
+import android.media.MediaPlayer;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    SeekBar timerSeekBar;
+    TextView timerTextView;
+    Boolean counterIsActive = false;
+    CountDownTimer countDownTimer;
+
+    public void updateTimer(int secondLefts) {
+
+        int minutes = secondLefts / 60;
+        int seconds = secondLefts - minutes * 60;
+
+        String min;
+        String sec;
+
+        if (minutes < 10) {
+            min = "0" + Integer.toString(minutes);
+        } else {
+            min = Integer.toString(minutes);
+        }
+
+        if (seconds < 10) {
+            sec = "0" + Integer.toString(seconds);
+        } else {
+            sec = Integer.toString(seconds);
+        }
+
+        timerTextView.setText(min + ":" + sec);
+    }
+
+    public void controlTimer(View view) {
+
+        ImageView startBtn = findViewById(R.id.startBtn);
+        ImageView stopBtn = findViewById(R.id.stopBtn);
+
+        if (counterIsActive == false) {
+
+            counterIsActive = true;
+            timerSeekBar.setEnabled(false);
+            startBtn.setVisibility(View.GONE);
+            stopBtn.setVisibility(View.VISIBLE);
+
+            countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    updateTimer((int) millisUntilFinished / 1000);
+                    //Log.i("timer", "s");
+                }
+
+                @Override
+                public void onFinish() {
+                    timerTextView.setText("00:00");
+                    MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.alarm);
+                    mp.start();
+                }
+            }.start();
+
+        } else {
+            timerTextView.setText("00:30");
+            timerSeekBar.setProgress(30);
+            countDownTimer.cancel();
+            timerSeekBar.setEnabled(true);
+            counterIsActive = false;
+
+            stopBtn.setVisibility(View.GONE);
+            startBtn.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timerSeekBar = findViewById(R.id.timerSeekBar);
+        timerTextView = findViewById(R.id.timerTextView);
+
+
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                updateTimer(progress);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) { }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) { }
+
+        });
+    }
+}
