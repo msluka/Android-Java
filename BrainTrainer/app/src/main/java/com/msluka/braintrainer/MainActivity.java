7530bdd209135b4b9c04e31908135d01db2606c1
Msluka <mamulashvilis@gmail.com>
2019-01-13 15:09:47 +0100

    Brain Trainer - Add Game is Active Boolean

--- BrainTrainer/app/src/main/java/com/msluka/braintrainer/MainActivity.java
+++ BrainTrainer/app/src/main/java/com/msluka/braintrainer/MainActivity.java
@@ -12,7 +12,7 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    boolean gameActive = true;
+    boolean gameActive = false;
 
     Button startButton;
     ArrayList<Integer> answers = new ArrayList<Integer>();
